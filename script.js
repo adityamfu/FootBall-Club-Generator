a2a97aab5eb792374e8ead857d3b51c0c611979a
adityamfu <akunpengganti.aditya@gmail.com>
2023-10-06 00:19:35 +0700

    add club league and create club filter

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 import { footballClubsData } from './database.js';
 
-let previousClubData = null; 
+let previousClubData = null;
+let selectedTier = [];
 
-function showInitialClub() {
-  const initialClubData = footballClubsData[0];
+function showInitialClub(selectedLeague, selectedTier) {
+  let initialClubData;
 
-  document.getElementById('clubName').textContent = `Your Club: ${initialClubData.name}`;
-  document.getElementById('clubLogo').src = `./images/${initialClubData.logo}`;
-  document.getElementById('clubLeague').textContent = `League: ${initialClubData.league}`;
+  if (selectedLeague === 'AllLeague' && selectedTier.length === 0) {
+    initialClubData = footballClubsData[0];
+  } else {
+    const clubsInSelectedLeagueAndTier = footballClubsData.filter((club) => (selectedLeague === 'AllLeague' || club.league === selectedLeague) && (selectedTier.length === 0 || selectedTier.includes(club.tier)));
+    initialClubData = clubsInSelectedLeagueAndTier[0];
+  }
+
+  if (initialClubData) {
+    document.getElementById('clubName').textContent = `Club: ${initialClubData.name}`;
+    document.getElementById('clubLogo').src = `./images/${initialClubData.logo}`;
+    document.getElementById('clubLeague').textContent = `League: ${initialClubData.league}`;
+    document.getElementById('clubTier').textContent = `Tier: ${initialClubData.tier}`;
+  }
 }
 
 const availableLeagues = Array.from(new Set(footballClubsData.map((club) => club.league)));
-
 const leagueSelect = document.getElementById('leagueSelect');
+const tierCheckboxes = document.querySelectorAll('input[name="tier"]');
 
 availableLeagues.forEach((league) => {
   const option = document.createElement('option');
@@ -21,15 +32,17 @@ availableLeagues.forEach((league) => {
   leagueSelect.appendChild(option);
 });
 
-showInitialClub();
+showInitialClub(leagueSelect.value, selectedTier);
 
-function generateRandomClub(selectedLeague) {
+function generateRandomClub(selectedLeague, selectedTier) {
+  document.getElementById('clubInfo').style.backgroundColor = '#151823';
+
   let filteredClubs;
 
-  if (selectedLeague === 'AllLeague') {
+  if (selectedLeague === 'AllLeague' && selectedTier.length === 0) {
     filteredClubs = footballClubsData;
   } else {
-    filteredClubs = footballClubsData.filter((club) => club.league === selectedLeague);
+    filteredClubs = footballClubsData.filter((club) => (selectedLeague === 'AllLeague' || club.league === selectedLeague) && (selectedTier.length === 0 || selectedTier.includes(club.tier)));
   }
 
   let randomClubData;
@@ -40,12 +53,17 @@ function generateRandomClub(selectedLeague) {
   } while (randomClubData === previousClubData);
 
   if (randomClubData) {
-    document.getElementById('clubName').textContent = `Your Club: ${randomClubData.name}`;
+    document.getElementById('clubName').textContent = `${randomClubData.name}`;
     document.getElementById('clubLogo').src = `./images/${randomClubData.logo}`;
-    document.getElementById('clubLeague').textContent = `League: ${randomClubData.league}`;
+    document.getElementById('clubLeague').textContent = `${randomClubData.league}`;
+    document.getElementById('clubTier').textContent = `${randomClubData.tier}`;
 
     previousClubData = randomClubData;
   }
+
+  setTimeout(() => {
+    document.getElementById('clubInfo').style.backgroundColor = '#9edc74';
+  }, 3500);
 }
 
 const generateButton = document.getElementById('generateButton');
@@ -53,13 +71,44 @@ const generateButton = document.getElementById('generateButton');
 generateButton.addEventListener('click', function () {
   const selectedLeague = leagueSelect.value;
   const duration = 3500;
-
   const interval = setInterval(() => {
-    generateRandomClub(selectedLeague);
+    generateRandomClub(selectedLeague, selectedTier);
   }, 120);
 
   setTimeout(() => {
     clearInterval(interval);
-    clubInfo.style.backgroundColor = '#007bff';
+    clubInfo.style.backgroundColor = '#9edc74';
   }, duration);
 });
+
+tierCheckboxes.forEach((checkbox) => {
+  checkbox.addEventListener('change', function () {
+    selectedTier = getSelectedTier();
+    showInitialClub(leagueSelect.value, selectedTier);
+  });
+});
+
+function getSelectedTier() {
+  const selectedTierCheckboxes = Array.from(tierCheckboxes).filter((checkbox) => checkbox.checked);
+  return selectedTierCheckboxes.map((checkbox) => checkbox.value);
+}
+
+leagueSelect.addEventListener('change', function () {
+  showInitialClub(leagueSelect.value, selectedTier);
+});
+
+var animateButton = function (e) {
+  e.preventDefault;
+  e.target.classList.remove('animate');
+
+  e.target.classList.add('animate');
+  setTimeout(function () {
+    e.target.classList.remove('animate');
+  }, 700);
+};
+
+var bubblyButtons = document.getElementsByClassName('bubbly-button');
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}
